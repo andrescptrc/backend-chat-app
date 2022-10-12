9f1ceaaff68313cb6b5fc27eea11e8d8a7330582
Andrés Carreño <andrebrayan74@gmail.com>
2022-10-12 10:17:14 -0500

    chore: add the base code

---
+++ src/controllers/auth.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from "express";
+
+export const login = (req: Request, res: Response) => {
+  res.status(200).json({
+    msg: "Login",
+  });
+};
---
+++ src/models/server.ts
@@ -0,0 +1,42 @@
+import express, { Application } from "express";
+import cors from "cors";
+
+import { DEFAULT_PORT } from "@constants/port";
+import { IPath } from "@interfaces/path";
+
+import authRoute from "@routes/auth";
+
+class Server {
+  app: Application;
+  PORT: string;
+  apiPaths: IPath;
+
+  constructor() {
+    this.app = express();
+    this.PORT = process.env.PORT || DEFAULT_PORT;
+    this.apiPaths = {
+      auth: "/api/auth",
+    };
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares(): void {
+    this.app.use(cors());
+
+    this.app.use(express.json());
+  }
+
+  routes(): void {
+    this.app.use(this.apiPaths.auth, authRoute);
+  }
+
+  listen(): void {
+    this.app.listen(this.PORT, () => {
+      console.log("Server running on the port: " + this.PORT);
+    });
+  }
+}
+
+export default Server;

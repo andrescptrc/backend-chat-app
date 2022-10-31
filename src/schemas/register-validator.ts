4e18929d26b5e7d7a61932608f293714b2a73701
Andres <andrebrayan74@gmail.com>
2022-10-31 18:38:31 -0500

    chore: fix the typo

--- src/schemas/register-validator.ts
+++ src/schemas/register-validator.ts
@@ -10,13 +10,13 @@ const registerSchema = Joi.object({
   }),
   email: Joi.string().email().required().messages({
     'string.base': 'The email must be a string.',
-    'string.empty': 'the email must not be empty.',
+    'string.empty': 'The email must not be empty.',
     'string.email': 'The email is not valid.',
     'any.required': 'The email is required.'
   }),
   password: Joi.string().min(8).max(16).required().messages({
     'string.base': 'The password must be a string.',
-    'string.empty': 'the password must not be empty.',
+    'string.empty': 'The password must not be empty.',
     'string.min': 'The password must have at least 8 characters.',
     'string.max': 'The password must not exceed the 16 characters.',
     'any.required': 'The password is required.'
